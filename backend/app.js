83defb6db87b276575f1f721a6052fbbba1b9621
CarrotRP <panharothlim@gmail.com>
2025-10-16 21:32:35 +0700

    express version

--- backend/app.js
+++ backend/app.js
@@ -173,7 +173,7 @@ app.use('/message', messageRoutes);
 if(process.env.NODE_ENV == 'production'){
     app.use(express.static(frontendPath));
 
-    app.get('*', (req, res) => {
+    app.get('/*', (req, res) => {
         res.sendFile(path.join(frontendPath, 'index.html'));
     })
 }
